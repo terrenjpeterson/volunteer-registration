4c0c449654c2e81f29c01b86c50e8208288c2654
Terren Peterson <terrenjpeterson@yahoo.com>
2020-04-18 17:38:39 +0000

    added put object for array

--- csvConverter.js
+++ csvConverter.js
@@ -3,40 +3,66 @@
 var aws = require('aws-sdk');
 
 exports.handler = (event, context, callback) => {
-    console.log('completed request');
+    console.log('converter initiated');
 
     var s3 = new aws.S3();
-            
+
+    // set parameters to where the source csv file is            
     var getParams = {Bucket : 'for-richmond-data', Key : 'volunteers.csv'};
-    //var getParams = {Bucket : 'for-richmond-data', Key : 'test.txt'};
-                    
+
+    // get the s3 data object based on the parameters
     s3.getObject(getParams, function(err, data) {
         if(err)
             console.log('Error getting csv data : ' + err);
         else {
-            //var returnData = data.Body;
             var stringData = data.Body.toString();
-            
-            //console.log("data :" + returnData);
-
+            // parse out each row in the csv file into an array
             var rows = stringData.split("\n");
-            var columns = rows[1].split(",");
-            var volunteer = {};
-                volunteer.createDate = columns[0];
-                volunteer.firstName = columns[1];
-                volunteer.lastName = columns[2];
-                volunteer.email = columns[3];
-                volunteer.phone = columns[4];
-                volunteer.addressStreet = columns[5];
-                volunteer.addressCity = columns[6];
-                volunteer.addressStateZipCountry = columns[7];
-                volunteer.church = columns[8];
-                volunteer.level = columns[9];
             
-            //console.log(columns);
-            console.log(volunteer);
+            // create master array object
+            var masterArray = [];
+            
+            // start for loop at 1 to skip the header with column names
+            for (var i = 1; i < rows.length; i++) {
+                // parse out the line in the csv file
+                var columns = rows[i].split(",");
+                
+                // create an object and associate column names to attributes for the object
+                var volunteer = {};
+                    volunteer.createDate = columns[0];
+                    volunteer.firstName = columns[1];
+                    volunteer.lastName = columns[2];
+                    volunteer.fullName = columns[1] + ' ' + columns[2];
+                    volunteer.email = columns[3];
+                    volunteer.phone = columns[4];
+                    volunteer.addressStreet = columns[5];
+                    volunteer.addressCity = columns[6];
+                    volunteer.addressStateZipCountry = columns[7];
+                    volunteer.church = columns[8];
+                    volunteer.level = columns[9];
 
-            console.log("Retrieved Data");
+                // push the new volunteer object into the array    
+                masterArray.push(volunteer); 
+            
+                //console.log(columns);
+                console.log("Volunteer " + i + " " + JSON.stringify(volunteer));
+            }
+            console.log("Full Array:" + JSON.stringify(masterArray));
+
+            console.log("Array Created with " + masterArray.length + " entries.");
+            
+            var postData = JSON.stringify(masterArray);
+            
+            var putParams = {Bucket : 'for-richmond-data',
+                            Key : 'volunteers.json',
+                            Body: postData};
+
+            s3.putObject(putParams, function(err, data) {
+                if(err)
+                    console.log('Error posting data' + err);
+                else
+                    console.log('Successfully posted data' + putParams.Body);
+            });
         }
     });
 };
